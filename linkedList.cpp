47b287e9e18b33067d6c74929f7d834f21539632
Mustafa Yalcinkaya <myalcinkaya136@gmail.com>
2023-03-10 11:59:08 +0300

    Added delete function to the linked list

--- linkedList.cpp
+++ linkedList.cpp
@@ -9,6 +9,39 @@ struct node{
     int data;
 };
 
+void deleteNode(struct node** head, int data){
+    if((*head)->data == data){
+        
+        struct node* temp = (*head);
+        (*head) = (*head)->next;
+        temp->next = NULL;
+        free(temp);
+        
+    }else{
+        
+        struct node* temp = (*head);
+        while(temp->next != NULL && temp->next->data != data) temp = temp -> next;
+        if(temp->next == NULL) {
+            cout << " The item you were looking for was not found  :  " << data << endl;
+            return;
+        }
+        else if(temp->next->data == data){
+            if(temp->next->next == NULL){
+                free(temp->next);
+                temp->next = NULL;
+            }
+            else{
+                struct node* temp2 = temp->next;
+                temp->next = temp2->next;
+                temp2->next = NULL;
+                free(temp2);
+            }
+        }
+        
+    }
+    
+}
+
 
 void addNode(struct node** head, int data){
     
@@ -16,10 +49,13 @@ void addNode(struct node** head, int data){
     temp -> data = data;
     
     if((*head) == NULL){
+        
         (*head) = temp;
         temp = NULL;
         free(temp);
+        
     }else{
+        
         struct node* iter = (*head);
         while(iter -> next != NULL) iter = iter -> next;
         iter -> next = temp;
@@ -27,10 +63,12 @@ void addNode(struct node** head, int data){
         temp = NULL;
         free(temp);
         free(iter);
+        
     }
 }
 
 void print(struct node* head){
+    cout << endl;
     while(head != NULL) {
         cout << head->data << " ";
         head = head->next;
@@ -45,6 +83,9 @@ int main()
     addNode(&head, 30);
     addNode(&head, 40);
     addNode(&head, 50);
+    addNode(&head, 100);
+    deleteNode(&head, 110);
+    deleteNode(&head, 120);
     
     print(head);
 
